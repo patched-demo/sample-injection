c8e2e1184af2e349cea0a67afcab12bd4ddc07ed
Patched Demo <161035990+patched-demo@users.noreply.github.com>
2024-07-26 07:46:19 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 import sqlite3
 
-# Simulated config file or a settings module
 CONFIG = {
     "default_table": "users",
     "default_column": "username"
 }
 
 def get_data_by_config_value(value):
-    # This might look suspicious due to string concatenation with values from CONFIG.
     query = "SELECT * FROM " + CONFIG["default_table"] + " WHERE " + CONFIG["default_column"] + " = '" + value + "'"
 
     connection = sqlite3.connect("database.db")
